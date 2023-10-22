794e26c33ac78604adbe75e60b66b13a6bb319f4
AsadSi <91189953+AsadSi@users.noreply.github.com>
2023-10-22 12:35:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+var timer_search_users = ''
+function search_users(){
+  clearTimeout(timer_search_users)
+  timer_search_users = setTimeout(async function(){ 
+    const frm = document.querySelector("#frm_search")
+    const url = frm.getAttribute('data-url')
+    console.log("URL: ", url)
+    const conn = await fetch(`/api/${url}`, {
+      method : "POST",
+      body : new FormData(frm)
+    })
+    const data = await conn.json()
+    document.querySelector("#query_results").innerHTML = ""
+    
+    data.forEach(user => {
+      let div_user = `
+        <div class="grid grid-cols-[100fr,100fr,50fr] p-2">
+          <div class="">${user.user_name}</div>
+          <div class="">${user.user_last_name}</div>
+          <div class="">${user.employee_salary}</div>
+        </div>
+      `
+      document.querySelector("#query_results").insertAdjacentHTML('afterbegin', div_user)
+    })    
+   }, 500)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
